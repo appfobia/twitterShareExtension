b3e2929b6ce63776fb1712d354b4cbda8161bb24
appfobia <soham_701@hotmail.com>
2016-04-29 21:59:48 +0530

    Done

--- background.js
+++ background.js
@@ -1,9 +1,25 @@
-chrome.contextMenus.create({
-title: "twitter share",
-contexts: ["selection"],
-onclick: myFunction
-});
+var contextsList = ["selection","link","image","page"];
 
-function myFunction(selectedText) {
-    chrome.tabs.create({url: "https://twitter.com/intent/tweet?text="+selectedText.selectionText});
+for(var i=0;i<contextsList.length;i++) {
+    var context= contextsList[i];
+    var titleX = "share this " + context + " on your profile";
+    chrome.contextMenus.create({title: titleX , contexts: [context] , onclick: myFunction , id: context});
+}
+
+function myFunction(data,tab) {
+    switch(data.menuItemId) {
+        case 'selection' :
+            chrome.tabs.create({url: "https://twitter.com/intent/tweet?text="+encodeURIComponent(data.selectionText)});
+            break;
+        case 'link' :
+            chrome.tabs.create({url: "https://twitter.com/intent/tweet?text="+encodeURIComponent(data.linkUrl)});
+            break;
+        case 'image' :
+            chrome.tabs.create({url: "https://twitter.com/intent/tweet?text="+encodeURIComponent(data.srcUrl)});
+            break;
+        case 'page' :
+            chrome.tabs.create({url: "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title) + "&url=" + data.pageUrl});
+            break;
+    }
+    
 }
